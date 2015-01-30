6f3a3b6f27e73fb7ec914853cfbdd850360ccfec
Zach Sitler <zachsitler@gmail.com>
2015-01-30 14:50:46 -0700

    Added a hacky way to print the steps to the solution

--- three-cannibals.go
+++ three-cannibals.go
@@ -10,19 +10,20 @@ const (
 	size   = 2
 	cstart = 3
 	mstart = 3
-	left   = 1
-	right  = -1
+	start  = 1
+	end    = -1
 )
 
 var Q *lang.Queue
 
 type state struct {
 	m, c, direction int
+	parent          *state
 }
 
 func main() {
 	Q = lang.NewQueue()
-	Q.Push(state{cstart, mstart, left})
+	Q.Push(state{cstart, mstart, start, nil})
 	for Q.Len() != 0 {
 		v := Q.Poll().(state)
 
@@ -30,17 +31,18 @@ func main() {
 		// fmt.Println(v)
 
 		if goalState(v) {
-			fmt.Println(v)
-			fmt.Println("done!")
+			fmt.Println("Solution found.")
+			fmt.Println("Steps:")
+			stateHistory(v)
 			break
 		}
 
-		getSuccessors(v)
+		getChildren(v)
 	}
 }
 
-// Get all possible adjacent nodes and attempt to add them to the queue.
-func getSuccessors(parent state) {
+// Get all possible moves we could make and attempt to add them to the queue.
+func getChildren(parent state) {
 	for c := 0; c <= size; c++ {
 		for m := 0; m <= size; m++ {
 			// skip the useless case
@@ -67,7 +69,8 @@ func addState(parent state, c, m int) {
 	direction := getDirection(parent.direction)
 
 	// This is a "move" from one side to the other
-	child := state{parent.m + m*direction, parent.c + c*direction, direction}
+	child := state{parent.m + m*direction, parent.c + c*direction,
+		direction, &parent}
 
 	if validState(child) {
 		Q.Push(child)
@@ -88,21 +91,36 @@ func validState(s state) bool {
 	return true
 }
 
-func getDirection(direction int) int {
-	if direction == left {
-		return right
-	}
-
-	return left
-}
-
 // This is our stop condition while searching. If we have a state
-// that has no cannibals/missionaries and the boat is on the right
+// that has no cannibals/missionaries and the boat is on the other side
 // side then we did it.
 func goalState(s state) bool {
-	if s.m+s.c != 0 || s.direction == left {
+	if s.m+s.c != 0 || s.direction == start {
 		return false
 	}
 
 	return true
+}
+
+func getDirection(direction int) int {
+	if direction == start {
+		return end
+	}
+
+	return start
+}
+
+// Horrible way of printing the history of the final state in reverse
+func stateHistory(s state) {
+	current := s
+	path := []string{}
+	for current.parent != nil {
+		curPath := fmt.Sprintf("M: %d, C: %d, direction: %d", current.m, current.c, current.direction)
+		path = append(path, curPath)
+		current = *current.parent
+	}
+
+	for i := len(path) - 1; i >= 0; i-- {
+		fmt.Println(path[i])
+	}
 }
