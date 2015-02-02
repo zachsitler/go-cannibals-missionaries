f881edf06f9488e38d374034cd57af6341c3f479
Zach Sitler <zachsitler@gmail.com>
2015-02-02 13:28:16 -0700

    Refactored and made the output a little nicer.

--- cannibals-missionaries.go
+++ cannibals-missionaries.go
@@ -16,33 +16,33 @@ const (
 
 var Q *lang.Queue
 
-type state struct {
-	m, c, direction int
-	parent          *state
-}
-
+// Run BFS on the state space and search for the steps to the solution
 func main() {
 	Q = lang.NewQueue()
 	Q.Push(state{cstart, mstart, start, nil})
 	for Q.Len() != 0 {
 		v := Q.Poll().(state)
 
-		// Uncomment to see nodes that were parsed
-		// fmt.Println(v)
-
 		if goalState(v) {
 			fmt.Println("Solution found.")
 			fmt.Println("Steps:")
-			stateHistory(v)
+			v.stateHistory()
 			break
 		}
 
-		getChildren(v)
+		v.getChildren()
 	}
 }
 
+// We only need the parent state for printing out the history of the steps.
+// Otherwise, we could do without it.
+type state struct {
+	m, c, direction int
+	parent          *state
+}
+
 // Get all possible moves we could make and attempt to add them to the queue.
-func getChildren(parent state) {
+func (s *state) getChildren() {
 	for c := 0; c <= size; c++ {
 		for m := 0; m <= size; m++ {
 			// skip the useless case
@@ -56,29 +56,16 @@ func getChildren(parent state) {
 			// We don't just want to keep undoing what we just did.
 			// For example, 1 0 and then 0 1 doesn't accomplish
 			// anything and we will be stuck in an infinite loop.
-			if (cstart-parent.c) == c && (mstart-parent.m) == m {
+			if (cstart-s.c) == c && (mstart-s.m) == m {
 				continue
 			}
-			addState(parent, c, m)
+			addState(s, c, m)
 		}
 	}
 }
 
-// Add a state to the queue if it is valid.
-func addState(parent state, c, m int) {
-	direction := getDirection(parent.direction)
-
-	// This is a "move" from one side to the other
-	child := state{parent.m + m*direction, parent.c + c*direction,
-		direction, &parent}
-
-	if validState(child) {
-		Q.Push(child)
-	}
-}
-
 // Ensure that a move doesn't kill any missionaries and doesn't break any rules
-func validState(s state) bool {
+func (s *state) validState() bool {
 	// Make sure we aren't breaking any obvious rules
 	if s.m > mstart || s.c > cstart || s.m < 0 || s.c < 0 {
 		return false
@@ -91,6 +78,53 @@ func validState(s state) bool {
 	return true
 }
 
+// We use -1/1 for the directions so we can easily add and remove
+// cannibals/missionaries when moving them.
+func (s *state) getDirection() int {
+	if s.direction == start {
+		return end
+	}
+
+	return start
+}
+
+func (s *state) stateHistory() {
+	current := s
+	path := []string{}
+	for current.parent != nil {
+		path = append(path, current.printState())
+		current = current.parent
+	}
+
+	for i := len(path) - 1; i >= 0; i-- {
+		fmt.Println(path[i])
+	}
+}
+
+func (s *state) printState() string {
+	result := fmt.Sprintf("M: %d, C: %d", s.m, s.c)
+	if s.direction == start {
+		result += " ----------------> "
+	} else {
+		result += " <---------------- "
+	}
+	result += fmt.Sprintf("M: %d, C: %d", mstart-s.m, cstart-s.c)
+	return result
+}
+
+// Add a state to the queue if it is valid.
+func addState(parent *state, c, m int) {
+	direction := parent.getDirection()
+
+	// This is a "move" from one side to the other.
+	child := state{parent.m + m*direction, parent.c + c*direction,
+		direction, parent}
+
+	if child.validState() {
+		Q.Push(child)
+	}
+}
+
 // This is our stop condition while searching. If we have a state
 // that has no cannibals/missionaries and the boat is on the other side
 // side then we did it.
@@ -100,26 +134,4 @@ func goalState(s state) bool {
 	}
 
 	return true
-}
-
-func getDirection(direction int) int {
-	if direction == start {
-		return end
-	}
-
-	return start
-}
-
-func stateHistory(s state) {
-	current := s
-	path := []string{}
-	for current.parent != nil {
-		curPath := fmt.Sprintf("M: %d, C: %d, direction: %d", current.m, current.c, current.direction)
-		path = append(path, curPath)
-		current = *current.parent
-	}
-
-	for i := len(path) - 1; i >= 0; i-- {
-		fmt.Println(path[i])
-	}
 }
